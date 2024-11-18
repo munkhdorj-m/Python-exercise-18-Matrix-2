c1ba7d331a90639d18d3ffaf80010b269f359ca7
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-11-18 10:35:54 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -1,36 +1,26 @@
 import pytest
 import inspect
-from assignment import subtract_matrices, sum_main_diagonal, sum_rows_columns
+from assignment import rotate_matrix_90_clockwise, search_in_matrix
 
 def check_contains_loop(function):
     source = inspect.getsource(function)
     return 'for' in source or 'while' in source
 
-@pytest.mark.parametrize("matrix1, matrix2, expected", [
-    ([[3, 5, 2], [7, 8, 6], [4, 1, 3]], [[1, 4, 0], [2, 3, 1], [3, 2, 1]], [[2, 1, 2], [5, 5, 5], [1, -1, 2]]),
-    ([[10, 8], [6, 4]], [[5, 3], [3, 2]], [[5, 5], [3, 2]]),
-    ([[12, 7, 9], [6, 5, 3], [4, 8, 10]], [[3, 5, 6], [2, 1, 3], [7, 2, 4]], [[9, 2, 3], [4, 4, 0], [-3, 6, 6]])
-])
-def test1(matrix1, matrix2, expected):
-    assert subtract_matrices(matrix1, matrix2) == expected
-    assert check_contains_loop(subtract_matrices)
-
 @pytest.mark.parametrize("matrix, expected", [
-    ([[4, 5, 6], [7, 8, 9], [1, 2, 3]], 15),
-    ([[1, 0, 0], [0, 1, 0], [0, 0, 1]], 3),
-    ([[3, 6, 9], [8, 5, 2], [1, 7, 4]], 12)
+    ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[7, 4, 1], [8, 5, 2], [9, 6, 3]]),
+    ([[5, 1], [3, 7]], [[3, 5], [7, 1]]),
+    ([[10, 20, 30, 40], [50, 60, 70, 80], [90, 100, 110, 120], [130, 140, 150, 160]], 
+     [[130, 90, 50, 10], [140, 100, 60, 20], [150, 110, 70, 30], [160, 120, 80, 40]])
 ])
-def test2(matrix, expected):
-    assert sum_main_diagonal(matrix) == expected
-    assert check_contains_loop(sum_main_diagonal)
+def test1(matrix, expected):
+    assert rotate_matrix_90_clockwise(matrix) == expected
+    assert check_contains_loop(rotate_matrix_90_clockwise)
 
-@pytest.mark.parametrize("matrix, expected_rows, expected_columns", [
-    ([[2, 4, 6], [1, 3, 5], [7, 9, 11]], [12, 9, 27], [10, 16, 22]),
-    ([[1, 1, 1], [2, 2, 2], [3, 3, 3]], [3, 6, 9], [6, 6, 6]),
-    ([[4, 0, 1], [9, 2, 3], [5, 6, 7]], [5, 14, 18], [18, 8, 11])
+@pytest.mark.parametrize("matrix, target, expected", [
+    ([[3, 8, 2], [5, 10, 7], [6, 1, 9]], 10, (1, 1)),
+    ([[4, 0, 1], [9, 2, 3], [5, 6, 7]], 6, (2, 1)),
+    ([[3, 5], [7, 1]], 8, "Not found")
 ])
-def test3(matrix, expected_rows, expected_columns):
-    rows, columns = sum_rows_columns(matrix)
-    assert rows == expected_rows
-    assert columns == expected_columns
-    assert check_contains_loop(sum_rows_columns)
+def test2(matrix, target, expected):
+    assert search_in_matrix(matrix, target) == expected
+    assert check_contains_loop(search_in_matrix)
