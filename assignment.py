31c13cb8e432fcbaca3161e252888b67265450df
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-11-18 10:33:48 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,16 +1,11 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def subtract_matrices(matrix1, matrix2):
+def rotate_matrix_90_clockwise(matrix):
     # Write your code here
     pass
 
 # Exercise 2
-def sum_main_diagonal(matrix):
-    # Write your code here
-    pass
-
-# Exercise 3
-def sum_rows_columns(matrix):
+def search_in_matrix(matrix, target):
     # Write your code here
     pass
